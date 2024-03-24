1a55b9a6878a57f9bc2bc2b38837bda858d2ea4d
Ben Meier <ben.meier@humanitec.com>
2024-03-24 22:59:15 +0000

    further work

--- libs/au/src/lib.rs
+++ libs/au/src/lib.rs
@@ -1,10 +1,13 @@
+use std::fmt::{Debug};
 use std::io::Error;
 use std::io::ErrorKind;
 
 use automerge::{Automerge, ObjType, ReadDoc, Value};
-use serde::{Serialize, Deserialize};
-use serde::de::Unexpected::Option;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
 use time::{Date, OffsetDateTime, Time};
+
+use crate::AuError::{IncorrectType, NoSuchKey};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
@@ -13,7 +16,7 @@ pub struct Item {
     pub class: Option<String>,
     pub content_type: String,
     pub content: Vec<u8>,
-    pub rank: i32,
+    pub rank: i64,
     pub deleted: bool,
     pub parent: Option<String>,
 }
@@ -38,6 +41,136 @@ pub struct Project {
     pub children: Vec<Item>,
 }
 
+#[derive(Error, Debug)]
+pub enum AuError {
+    #[error("no such key")]
+    NoSuchKey(String),
+    #[error("incorrect type - expected {1}")]
+    IncorrectType(String, String),
+    #[error("'{0}': {1}")]
+    NestedError(String, Box<dyn std::error::Error>)
+}
+
+fn decode_string(source: &Automerge, node: &automerge::ObjId, k: &str) -> Result<Option<String>, Box<dyn std::error::Error>> {
+    match source.get(node, k) {
+        Err(e) => Err(Box::new(e)),
+        Ok(Some((Value::Scalar(v), _))) => {
+            if !v.is_str() {
+                return Err(Box::new(IncorrectType(String::from(k), String::from("string"))))
+            }
+            return Ok(Some(v.to_string()))
+        },
+        _ => Ok(None),
+    }
+}
+
+fn decode_bool(source: &Automerge, node: &automerge::ObjId, k: &str) -> Result<Option<bool>, Box<dyn std::error::Error>> {
+    match source.get(node, k) {
+        Err(e) => Err(Box::new(e)),
+        Ok(Some((Value::Scalar(v), _))) => {
+            if !v.is_boolean() {
+                return Err(Box::new(IncorrectType(String::from(k), String::from("bool"))))
+            }
+            return Ok(v.to_bool())
+        },
+        _ => Ok(None),
+    }
+}
+
+fn decode_i64(source: &Automerge, node: &automerge::ObjId, k: &str) -> Result<Option<i64>, Box<dyn std::error::Error>> {
+    match source.get(node, k) {
+        Err(e) => Err(Box::new(e)),
+        Ok(Some((Value::Scalar(v), _))) => {
+            if !v.is_int() {
+                return Err(Box::new(IncorrectType(String::from(k), String::from("i64"))))
+            }
+            return Ok(v.to_i64())
+        },
+        _ => Ok(None),
+    }
+}
+
+fn decode_timestamp(source: &Automerge, node: &automerge::ObjId, k: &str) -> Result<Option<OffsetDateTime>, Box<dyn std::error::Error>> {
+    match source.get(node, k) {
+        Err(e) => Err(Box::new(e)),
+        Ok(Some((Value::Scalar(v), _))) => {
+            if !v.is_timestamp() {
+                return Err(Box::new(IncorrectType(String::from(k), String::from("timestamp"))))
+            }
+            return match OffsetDateTime::from_unix_timestamp_nanos(v.to_i64().unwrap() as i128 * 1_1000_000) {
+            Ok(t) => Ok(Some(t)),
+            Err(_) => Err(Box::new(IncorrectType(String::from(k), String::from("timestamp")))),
+            }
+        },
+        _ => Ok(None),
+    }
+}
+
+fn decode_content(source: &Automerge, node: &automerge::ObjId, k :&str) -> Result<Option<Vec<u8>>, Box<dyn std::error::Error>> {
+    return match source.get(node, "content") {
+        Ok(Some((Value::Object(ObjType::Text), node))) => {
+            match source.text(&node) {
+                Ok(v) => {
+                    Ok(Some(Vec::from(v)))
+                },
+                Err(_) => {
+                    Err(Box::new(IncorrectType(String::from(k), String::from("text"))))
+                }
+            }
+        },
+        Ok(Some((Value::Scalar(value), _))) => {
+            match value.to_bytes() {
+                Some(b) => {
+                    Ok(Some(Vec::from(b)))
+                },
+                None => {
+                    Err(Box::new(IncorrectType(String::from(k), String::from("text"))))
+                }
+            }
+        }
+        _ => Ok(None),
+    }
+}
+
+pub fn decode_item(source: &Automerge, items_node: &automerge::ObjId, k: String) -> Result<Option<Item>, Box<dyn std::error::Error>> {
+
+    let mut new_item = Item::default();
+    new_item.id = k.clone();
+
+    let item_node = match source.get(items_node, k) {
+        Ok(None) => {
+            return Ok(None)
+        },
+        Err(e) => Err(Box::new(e)),
+        Ok(Some((_, n))) => Ok(n),
+    }?;
+
+    new_item.parent = decode_string(&source, &item_node, "parent")?;
+    new_item.class = decode_string(&source, &item_node, "class")?;
+    new_item.content_type = match decode_string(&source, &item_node, "content_type")? {
+        None => {
+            return Err(Box::new(NoSuchKey(String::from("content_type"))))
+        },
+        Some(v) => v,
+    };
+    new_item.deleted = decode_bool(&source, &item_node, "deleted")?.unwrap_or(false);
+    new_item.rank = decode_i64(&source, &item_node, "rank")?.unwrap_or(0);
+    new_item.at = match decode_timestamp(&source, &item_node, "at")? {
+        None => {
+            return Err(Box::new(NoSuchKey(String::from("at"))))
+        },
+        Some(v) => v,
+    };
+    new_item.content = match decode_content(&source, &item_node, "content")? {
+        None => {
+            return Err(Box::new(NoSuchKey(String::from("content"))))
+        },
+        Some(v) => v,
+    };
+
+    return Ok(Some(new_item))
+}
+
 pub fn decode(source: &Automerge) -> Result<Project, Box<dyn std::error::Error>> {
     let items_node = match source.get(automerge::ROOT, "items") {
         Ok(Some((Value::Object(ObjType::Map), node))) => node,
@@ -48,74 +181,12 @@ pub fn decode(source: &Automerge) -> Result<Project, Box<dyn std::error::Error>>
 
     let keys = source.keys(&items_node);
     for k in keys {
-        let item_node = match source.get(&items_node, k.clone()) {
-            Ok(Some((Value::Object(ObjType::Map), node))) => node,
-            _ => return Err(Box::new(Error::new(ErrorKind::InvalidData,  format!("missing 'items' {} or is not a map", k)))),
-        };
-        let mut new_item = Item::default();
-        new_item.id = String::from(&k);
-
-        match source.get(&item_node, "at") {
-            Ok(Some((Value::Scalar(value), _))) => {
-                if value.is_timestamp() {
-                    match OffsetDateTime::from_unix_timestamp_nanos(value.to_i64().unwrap() as i128 * 1_000_000) {
-                        Ok(v) => {
-                            new_item.at = v;
-                        },
-                        _ => return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} at is not a valid timestamp", k)))),
-                    }
-                }
-            }
-            _ => return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} is missing 'at'", k))))
+        let new_item = decode_item(source, &items_node, k.clone())
+            .map_err(|e| Box::new(AuError::NestedError(k.clone(), e)))?;
+        if new_item.is_none() {
+            return Err(Box::new(NoSuchKey(k.clone())))
         }
-
-        match source.get(&item_node, "content_type") {
-            Ok(Some((Value::Scalar(value), _))) => {
-                if value.is_str() {
-                    new_item.content_type = value.to_string()
-                } else {
-                    return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} content_type is not a string", k))))
-                }
-            }
-            _ => return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} is missing 'content_type'", k))))
-        }
-
-        match source.get(&item_node, "content") {
-            Ok(Some((Value::Object(ObjType::Text), node ))) => {
-                match source.text(&node) {
-                    Ok(v) => {
-                        new_item.content = Vec::from(v)
-                    },
-                    Err(e) => {
-                        return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} invalid text content: {}", k, e))))
-                    }
-                }
-            },
-            Ok(Some((Value::Scalar(value), _))) => {
-                match value.to_bytes() {
-                    Some(b) => {
-                        new_item.content = Vec::from(b)
-                    },
-                    _ => {
-                        return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} invalid byte content", k))))
-                    }
-                }
-            }
-            _ => return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} is missing 'content'", k))))
-        }
-
-        match source.get(&item_node, "parent") {
-            Ok(Some((Value::Scalar(value), _))) => {
-                if value.is_str() {
-                    new_item.parent = Option::Some(value.to_string())
-                } else {
-                    return Err(Box::new(Error::new(ErrorKind::InvalidData, format!("item {} parent is not a string", k))))
-                }
-            },
-            _ => {}
-        }
-
-        out.push(new_item)
+        out.push(new_item.unwrap())
     }
 
     return Ok(Project{children: out})
